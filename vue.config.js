224f7247f3e71dd17f0197eade6cce97f2743162
augustobernardo <gutooliveira027@gmail.com>
2023-03-21 09:49:54 -0300

    Adicionando possíveis ícones para o pwa

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
+  // configureWebpack: {
+  //   devtool: 'source-map'
+  // },
+  // chainWebpack: config => {
+  //   config.module
+  //     .rule('vue')
+  //     .use('vue-loader')
+  //     .loader('vue-loader')
+  //     .tap(options => {
+  //       options.compilerOptions = {
+  //         ...options.compilerOptions,
+  //         isCustomElement: tag => tag.startsWith('ion-')
+  //       }
+  //       return options
+  //     })
+  // },
+
 })
